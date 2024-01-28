6cf47be24e4964b1d56125a7b48acd2a04487c06
zk-VerITAS <157987029+zk-VerITAS@users.noreply.github.com>
2024-01-28 11:02:04 -0800

    Add files via upload

---
+++ benches/msm_bench.rs
@@ -0,0 +1,61 @@
+#[macro_use]
+extern crate criterion;
+
+use ark_ec::msm::VariableBaseMSM as ArkworksMSM;
+use ark_ec::CurveGroup;
+use ark_ec::ProjectiveCurve;
+use ark_std::test_rng;
+use ark_std::UniformRand;
+use criterion::{BenchmarkId, Criterion};
+
+// use curve25519_dalek::ristretto::RistrettoPoint;
+// use curve25519_dalek::scalar::Scalar;
+// use curve25519_dalek::traits::MultiscalarMul;
+
+use ark_bls12_381::G1Projective;
+
+fn bench_msm(c: &mut Criterion) {
+    let rng = &mut test_rng();
+
+    let mut group = c.benchmark_group("msm");
+    for d in 12..17 {
+        let size = 1 << d;
+        let scalars = (0..size)
+            .map(|_| ark_bls12_381::Fr::rand(rng))
+            .collect::<Vec<_>>();
+        let bases = (0..size)
+            .map(|_| ark_bls12_381::G1Projective::rand(rng).into_affine())
+            .collect::<Vec<_>>();
+
+        group
+            .sample_size(10)
+            .bench_with_input(BenchmarkId::new("arkworks", d), &d, |b, _| {
+                b.iter(|| G1Projective::msm(&bases, &scalars))
+            });
+
+        // group
+        //     .sample_size(10)
+        //     .bench_with_input(BenchmarkId::new("chunks", d), &d, |b, _| {
+        //         b.iter(|| msm_chunks::<G1Affine, F, _, _>(&bases.as_slice(), &scalars.as_slice()))
+        //     });
+
+        // group.bench_with_input(BenchmarkId::new("dalek", d), &d, |b, &d| {
+        //     let size = 1 << d;
+        //     let scalars = (0..size)
+        //         .map(|_| Scalar::random(&mut OsRng))
+        //         .collect::<Vec<_>>();
+        //     let bases = (0..size)
+        //         .map(|_| RistrettoPoint::random(&mut OsRng))
+        //         .collect::<Vec<_>>();
+        //     b.iter(|| RistrettoPoint::multiscalar_mul(scalars.iter(), bases.iter()))
+        // });
+    }
+}
+
+criterion_group! {
+    name=msm_benchmarks;
+    config=Criterion::default();
+    targets=bench_msm,
+}
+
+criterion_main! {msm_benchmarks}
---
+++ benches/proofs_bench.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate criterion;
+extern crate merlin;
+
+use criterion::{BenchmarkId, Criterion};
+
+use ark_bls12_381::Fr;
+use ark_ff::One;
+use ark_poly::univariate::DensePolynomial;
+use ark_poly::DenseUVPolynomial;
+
+use ark_gemini::subprotocols::sumcheck::proof::Sumcheck;
+
+fn bench_sumcheck(c: &mut Criterion) {
+    let mut group = c.benchmark_group("sumcheck");
+
+    for d in 12..17 {
+        group.bench_with_input(BenchmarkId::new("time", d), &d, |b, &d| {
+            let rng = &mut ark_std::test_rng();
+
+            b.iter(|| {
+                let f = DensePolynomial::<Fr>::rand(1 << d, rng).coeffs;
+                let g = DensePolynomial::<Fr>::rand(1 << d, rng).coeffs;
+                let mut transcript = merlin::Transcript::new(b"LTAPS");
+                Sumcheck::new_time(&mut transcript, &f, &g, &Fr::one());
+            });
+        });
+
+        group.bench_with_input(BenchmarkId::new("space", d), &d, |b, &d| {
+            let rng = &mut ark_std::test_rng();
+
+            let f = DensePolynomial::<Fr>::rand(1 << d, rng);
+            let g = DensePolynomial::<Fr>::rand(1 << d, rng);
+            let mut f_rev = f.coeffs().to_vec();
+            let mut g_rev = g.coeffs().to_vec();
+            f_rev.reverse();
+            g_rev.reverse();
+            let f_stream = f_rev.as_slice();
+            let g_stream = g_rev.as_slice();
+
+            b.iter(|| {
+                let mut transcript = merlin::Transcript::new(b"LTAPS");
+                Sumcheck::new_space(&mut transcript, f_stream, g_stream, Fr::one());
+            });
+        });
+    }
+}
+
+criterion_group! {
+    name=proofs_benchmarks;
+    config=Criterion::default();
+    targets=
+        bench_sumcheck,
+}
+
+criterion_main! {proofs_benchmarks}
